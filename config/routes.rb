2db37d133f9a7dba656d08e61abb0bed1047eb37
danielvebo90 <danielvebo@gmail.com>
2020-03-11 19:33:20 -0500

    crear funcionalida para created product

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :products
 
   #get '/products/new', to: 'products#new'
+  #post "products", to: 'products#create'
+  #get '/products/new', to: 'products#new', as: 'new_product'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
