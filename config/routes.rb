71c41160470a4b0f7bc8479374e2db853ed4b481
danielvebo90 <danielvebo@gmail.com>
2020-03-11 19:38:26 -0500

    crear funcionalidad mostrar producto

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   #get '/products/new', to: 'products#new'
   #post "products", to: 'products#create'
   #get '/products/new', to: 'products#new', as: 'new_product'
+  #get '/products/:id', to: 'products#show'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
