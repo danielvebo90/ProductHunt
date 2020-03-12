7ca01004eba3819cd7be0aea3396561da86cd5ea
danielvebo90 <danielvebo@gmail.com>
2020-03-11 19:48:10 -0500

    Editar Producto

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,6 +15,15 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path, notice: "El producto ha sido modificado con éxito"
+    else
+      render :edit
+    end
+  end
+
   def create
     @product = Product.new(product_params)
     if @product.save
